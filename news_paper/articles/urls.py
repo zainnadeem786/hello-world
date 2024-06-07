895803b7ba8540c4401e36bb977bef3466b9e865
zainnadeem786 <zainnadeemzainnadeem80@gmail.com>
2024-06-07 01:38:25 -0700

    News App

--- news_paper/articles/urls.py
+++ news_paper/articles/urls.py
@@ -1,5 +1,10 @@
 from django.urls import path
-from .views import ArticleListView
+from .views import ArticleListView ,ArticleDetailView ,ArticleUpdateView , ArticleDeleteView ,ArticleCreateView # new
+
 urlpatterns = [
- path("", ArticleListView.as_view(), name="article_list"),
+    path("<int:pk>/", ArticleDetailView.as_view(),name="article_detail"), # new
+    path("<int:pk>/edit/", ArticleUpdateView.as_view(),name="article_edit"), # new
+    path("<int:pk>/delete/", ArticleDeleteView.as_view(),name="article_delete"), # new
+    path("new/", ArticleCreateView.as_view(), name="article_new"), # new
+    path("", ArticleListView.as_view(), name="article_list"),
 ]
